cf0cd51a5c884efef87a93a7c8f4ac97f2dd43a7
Ian Allison <iana@pims.math.ca>
2018-07-27 16:53:35 -0700

    Adding skeleton for request-sharder application
    Signed-off-by: Ian Allison <iana@pims.math.ca>

--- request-sharder.py
+++ request-sharder.py
@@ -1,6 +1,31 @@
-#!/usr/bin/env python3
+from tornado import log, web
+import tornado.ioloop
+import tornado.web
 
-from concurrent.futures import ThreadPoolExecutor
+class ShardHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
 
-from sharder import Sharder
+def make_app():
+    return tornado.web.Application([
+        (r"/", ShardHandler),
+    ])
 
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sharder import Sharder, Base
+
+    log.enable_pretty_logging()
+
+    # Give ourselves a database to play with. This should probably become an
+    # application configuration item e.g. --sqlite, --posgtres. Anything that
+    # returns an engine should work
+    engine = create_engine('sqlite:///:memory:', echo=True)
+
+    sharder_buckets = [f'hub-{hub}' for hub in range(5)]
+
+    sharder = Sharder(engine, 'hub', sharder_buckets)
+
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
